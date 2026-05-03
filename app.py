cf8d46254ece32ae1e620405206501d109945e5b
sayyedaakibali6-sketch <sayyedaakibali6@gmail.com>
2026-05-03 16:48:51 +0400

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,16 @@ import requests
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pymongo
-from bson.objectid import ObjectId
 
 akki = Flask(__name__)
 CORS(akki)
 
+# MongoDB Connection
 client = pymongo.MongoClient("mongodb+srv://abudhabisyed80_db_user:Akki12345@cluster0.cdettyo.mongodb.net/fitnessDB?retryWrites=true&w=majority")
 db = client.fitnessDB
 reels_col = db.reels
 
+# Tumhari API Key
 YT_API_KEY = "AIzaSyBVerjaQcUumGBOSO--M1B4bOFUgXjc8eM"
 
 @akki.route('/api/upload', methods=['POST'])
@@ -18,25 +19,29 @@ def upload():
     try:
         data = request.json
         url = data.get("video_url")
-        # Video ID nikalne ka sabse fast tareeka
-        v_id = url.split("shorts/")[1].split("?")[0] if "shorts/" in url else url.split("v=")[1].split("&")[0] if "v=" in url else url.split("/")[-1]
+        # Video ID nikalne ka logic
+        if "shorts/" in url:
+            v_id = url.split("shorts/")[1].split("?")[0]
+        elif "v=" in url:
+            v_id = url.split("v=")[1].split("&")[0]
+        else:
+            v_id = url.split("/")[-1].split("?")[0]
 
         # YouTube Data Fetch
         api_url = f"https://www.googleapis.com/youtube/v3/videos?id={v_id}&key={YT_API_KEY}&part=statistics,snippet"
         res = requests.get(api_url).json()
         
-        # Agar API response na de, toh default data bhar do (Crash nahi hoga)
         if 'items' in res and len(res['items']) > 0:
             item = res['items'][0]
             title = item['snippet']['title']
             likes = item['statistics'].get('likeCount', '0')
             channel = item['snippet']['channelTitle']
         else:
-            title, likes, channel = "YouTube Video", "M", "Vanced User"
+            title, likes, channel = "YouTube Video", "0", "Vanced User"
 
         reels_col.insert_one({
             "video_id": v_id,
-            "video_url": f"https://www.youtube.com/embed/{v_id}?autoplay=1&modestbranding=1&rel=0&iv_load_policy=3&enablejsapi=1&playsinline=1",
+            "video_url": f"https://www.youtube.com/embed/{v_id}?autoplay=1&modestbranding=1&rel=0&enablejsapi=1&playsinline=1",
             "caption": title,
             "likes": likes,
             "channel_name": channel
